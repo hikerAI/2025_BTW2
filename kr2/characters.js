f72cd09af1c8273fa1b5a32b0c574cbd05ac5c28
hikerAI <hiker7013@gmail.com>
2025-12-03 15:34:45 +0500

    Update and rename players.js to characters.js

--- kr2/characters.js
+++ kr2/characters.js
@@ -1,13 +1,13 @@
-async function getResponce() {
-    let responce = await fetch("players.json")
-    let content = await responce.text()
+async function getResponse() {
+    let response = await fetch("characters.json")
+    let content = await response.text()
     content = JSON.parse(content)
 
-    let node_for_insert = document.getElementById("node_for_insert")
-    node_for_insert.innerHTML = '' // Очищаем контейнер
+    let nodeForInsert = document.getElementById("node_for_insert")
+    nodeForInsert.innerHTML = '' 
 
     content.forEach((item, key) => {
-        node_for_insert.innerHTML += `
+        nodeForInsert.innerHTML += `
         <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
             <div class="card h-100 d-flex flex-column">
                 <img class="card-img-top responsive-img mt-2" 
@@ -17,7 +17,7 @@ async function getResponce() {
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${item.title}</h5>
                     <p class="card-text flex-grow-1">${item.description}</p>
-                    <p class="card-text"><strong>Номер игрока: ${item.number}</strong></p>
+                    <p class="card-text"><strong>Номер персонажа: ${item.number}</strong></p>
                     <input type="hidden" name="vendor_code" value="${item.vendor_code}">
                     <div class="mt-auto">
                     </div>
@@ -27,4 +27,4 @@ async function getResponce() {
         `
     })
 }
-getResponce()
+getResponse()
